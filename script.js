6d6b80536775e00adbf722b06f12114a386436a0
Merwann <merwannheret@gmail.com>
2025-07-23 14:03:56 +0200

    pagination

--- script.js
+++ script.js
@@ -9,40 +9,44 @@ const options = {
 const TESTAFFICHAGE = document.getElementById('testAffichage')
 const PREVIOUSBUTTON = document.getElementById('previousButton') // pour aller à la page d'avant
 const NEXTBUTTON = document.getElementById('nextButton') // pour aller à la page d'après
+const CURRENTPAGE = document.getElementById('currentPage')
+let page = 1
+let totalPages = 100
 
 async function showHomePage(filmData) {
-  for(let i=0;i<filmData.results.length;i++){
-  const container = document.createElement('div');
-  container.className = "card"
+  TESTAFFICHAGE.innerHTML = ""
+  for (let i = 0; i < filmData.results.length; i++) {
+    const container = document.createElement('div');
+    container.className = "card"
 
-  const cardImage = document.createElement('div');
-  cardImage.className= "card-image"
-
-
-  const img = document.createElement('img');
-  img.src = await urlImage(filmData.results[i].poster_path);
-  // img.width = 250
-  img.alt = 'afficheFilm';
-  img.className = "imageFilm"
-
-  cardImage.appendChild(img);
-
-  const cardHeader = document.createElement('div');
-  cardHeader.className = "card-content"
+    const cardImage = document.createElement('div');
+    cardImage.className = "card-image"
 
 
-  const title = document.createElement('p');
-  title.className = "media-content"
-  title.textContent = filmData.results[i].title;
+    const img = document.createElement('img');
+    img.src = await urlImage(filmData.results[i].poster_path);
+    // img.width = 250
+    img.alt = 'afficheFilm';
+    img.className = "imageFilm"
 
-  cardHeader.appendChild(title);
+    cardImage.appendChild(img);
+
+    const cardHeader = document.createElement('div');
+    cardHeader.className = "card-content"
 
 
-  container.appendChild(cardImage);
-  container.appendChild(cardHeader);
-  
+    const title = document.createElement('p');
+    title.className = "media-content"
+    title.textContent = filmData.results[i].title;
 
-  TESTAFFICHAGE.appendChild(container);
+    cardHeader.appendChild(title);
+
+
+    container.appendChild(cardImage);
+    container.appendChild(cardHeader);
+
+
+    TESTAFFICHAGE.appendChild(container);
   }
 }
 
@@ -59,13 +63,43 @@ async function infoFilm(id) {
 
 
 
-async function trendingMovies(page){
-  const RESPONSE = await fetch (`https://api.themoviedb.org/3/trending/movie/day?language=fr-FR&page=${page}`, options)
+async function trendingMovies(page) {
+  const RESPONSE = await fetch(`https://api.themoviedb.org/3/trending/movie/day?language=fr-FR&page=${page}`, options)
   const FILMS = await RESPONSE.json()
   return FILMS
 }
 
-showHomePage(await trendingMovies(4))
+showHomePage(await trendingMovies(page))
+
+
 
 //------------------------------------------------------------------------------------------------------------------------------------
 // on doit ajouter des addEventListener('click', *fonction*) sur les boutons previews et next
+
+NEXTBUTTON.addEventListener("click", async () => {
+  if (page < totalPages) {
+    page++
+    let dataPage = await trendingMovies(page)
+    showHomePage(dataPage)
+    CURRENTPAGE.innerText=page
+  } else {
+    page = 1
+    let dataPage = await trendingMovies(page)
+    showHomePage(dataPage)
+    CURRENTPAGE.innerText=page
+  }
+})
+
+PREVIOUSBUTTON.addEventListener("click", async () => {
+  if (page > 1) {
+    page --
+    let dataPage = await trendingMovies(page)
+    showHomePage(dataPage)
+    CURRENTPAGE.innerText=page
+  } else {
+    page = totalPages
+    let dataPage = await trendingMovies(page)
+    showHomePage(dataPage)
+    CURRENTPAGE.innerText=page
+  }
+})
