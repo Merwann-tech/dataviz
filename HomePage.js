f8ba50b6e21575c02926ba5877a377348055faba
Hellyes31 <elies.auguet@gmail.com>
2025-07-24 15:12:09 +0200

    Ptitemodif

--- HomePage.js
+++ HomePage.js
@@ -81,7 +81,7 @@ async function showHomeHeader(filmData) {
   headerDiv.className = "header-image";
 
   let infoMovie = await infoFilm(filmData.results[slide].id);
-//Background du header est le backdrop du film, avec du style
+// Background du header est le backdrop du film, avec du style
   headerDiv.style.backgroundImage = `linear-gradient(to right,
     rgba(31,31,31,0.2) 0%,
     rgba(31,31,31,0.5) 30%,
