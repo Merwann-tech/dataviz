5c5e98f63e40127095667b60ca13d8cbbb5dd1f6
Werner <werner.wolff1109@gmail.com>
2025-07-29 11:50:26 +0200

    ajout deuxième graphe

--- graph.js
+++ graph.js
@@ -8,14 +8,129 @@ const options = {
   },
 }
 
-//*******************************************************************************************************************************************************
+//********************************************************************Variables globales*****************************************************************
 
 let chart // on définit une variable chart qui va contenir notre affichage ( c'est la chart qu'on va créer toutes les x secondes )
 let yearToShow = 2014 // on l'a met en variable globale car on doit pouvoir la modifier lors de notre appel du setInterval. --> démarre l'affichage en 2015
 const CURRENTYEAR = new Date() // on récupère la date système
 const ACTUALYEAR = CURRENTYEAR.getFullYear() // on récupère l'année de la date système
 
-//*******************************************************************************************************************************************************
+
+//***************************************************************************GRAPHE 2********************************************************************
+
+//L'évenement DOMContentLoader se lance au moment ou le fichier est chargé
+document.addEventListener('DOMContentLoaded', function () {
+  const DATAGRAPH2 = {
+    labels: ["Action", "Aventure", "Animation", "Comédie", "Crime", "Documentaire", "Drame", "Familial", "Fantastique", "Histoire", "Horreur", "Musique", "Mystère", "Romance", "Science-Fiction", "Téléfilm", "Thriller", "Guerre", "Western"],
+    datasets: [{
+      label: 'Genre des films',
+
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // on le fait démarrer à 0 pour qu'on ait un affichage ou il n'y a rien
+
+      backgroundColor: ['#f87171', '#fb923c', '#facc15', '#4ade80', '#60a5fa', '#3339ff', '#ff33d1', '#39ff33', '#7a33ff', '#33fff3', '#d8e80b', '#8f3192', '#F54927', '#B122AF', '#C260BF', '#496EBD', '#C2A2DF', '#7A5111', '#E9B526'],
+    }]
+  }
+
+  const configGenre = {
+    type: 'doughnut',
+    data: DATAGRAPH2,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'bottom',
+        },
+        title: {    
+          display: true,
+          text: 'Genre des films de cette page'
+        }
+      }
+    },
+  }
+
+  const DISPLAYGRAPHGENRE = document.getElementById('displayGraphGenre')
+  chart = new Chart(DISPLAYGRAPHGENRE, configGenre)
+})
+
+
+//méthodo qui permet de récupérer les informations précises de la page
+async function getPageGenre(categorieToShow, PageToShow){
+  const genreCounts = new Array(19).fill(0) // on créer un tableau de 19 elements et on les remplit de 0 pour éviter qu'ils soient indéfinis
+  const response = await fetch(`https://api.themoviedb.org/3/${categorieToShow}/movie/day?language=fr-FR&page=${PageToShow}`, options)
+  const ANSWER = await response.json()
+  console.log(ANSWER.results)
+  return ANSWER
+}
+
+// Méthodo qui va permettre de créer le tableau de données de la page
+// récupère les infos de ANSWER.results et va les trier dans un tableau de genre ( les genres ont des id )
+async function sortDataGenre(dataPage){
+
+}
+
+async function updateChartwithGenre(categorieToShow, pageToShow){
+  const arrayInfo = await getPageGenre(categorieToShow, pageToShow) // des informations qu'on retrouve dans les addEventListener présent dans les lignes d'avant ( a voir si il faut pas lancer la fonction depuis l'autre fichier)
+  
+  // permet de transférer les données de notre array dans notre chart
+  for (let i = 0; i < 19; i++) {
+    chart.data.datasets[0].data[i] = arrayInfo[i]
+  }
+
+  chart.update() // va relancer la génération du graphe
+}
+
+
+//***************************************************************************GRAPHE 1********************************************************************
+
+//Premier graphe : Films sortis par mois et par année
+//L'évenement DOMContentLoader se lance au moment ou le fichier est chargé
+document.addEventListener('DOMContentLoaded', function () {
+  const DATAGRAPH1 = {
+    labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+    datasets: [{
+      label: 'Nombre de sorties',
+
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // on le fait démarrer à 0 pour qu'on ait un affichage ou il n'y a rien
+
+      backgroundColor: ['#f87171', '#fb923c', '#facc15', '#4ade80', '#60a5fa', '#3339ff', '#ff33d1', '#39ff33', '#7a33ff', '#33fff3', '#d8e80b', '#8f3192'],
+    }]
+  }
+
+  const config = {
+    type: 'doughnut',
+    data: DATAGRAPH1,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {    
+          display: true,
+          text: 'Films sortis par mois par année'
+        }
+      }
+    },
+  }
+
+  const DISPLAYGRAPH = document.getElementById('displayGraph')
+  chart = new Chart(DISPLAYGRAPH, config)
+
+  updateChartWithRealData() // on lance la fonction une première fois pour que le graphe se charge une première fois sans que ça reste vide
+
+  let interval = setInterval(() => {
+    yearToShow++
+
+    if(yearToShow === ACTUALYEAR){
+        clearInterval(interval)
+        console.log("Fin de l'affichage")
+    }
+
+    updateChartWithRealData()
+  }, 11600) // pour que ça se synchronise avec avec l'effet du slider
+})
+
+//***********************************************************************Fonctions graphe 1******************************************************************
 
 //Permet de récupérer les différentes informations des pages de l'api
 async function dataFilm(yearToShow) {
@@ -68,52 +183,3 @@ async function updateChartWithRealData() {
   chart.options.plugins.title.text = `Sorties de films en ${yearToShow}`
   chart.update()
 }
-
-//*******************************************************************************************************************************************************
-
-//L'évenement DOMContentLoader se lance au moment ou le fichier est chargé
-document.addEventListener('DOMContentLoaded', function () {
-  const data = {
-    labels: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
-    datasets: [{
-      label: 'Nombre de sorties',
-
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // on le fait démarrer à 0 pour qu'on ait un affichage ou il n'y a rien
-
-      backgroundColor: ['#f87171', '#fb923c', '#facc15', '#4ade80', '#60a5fa', '#3339ff', '#ff33d1', '#39ff33', '#7a33ff', '#33fff3', '#d8e80b', '#8f3192'],
-    }]
-  }
-
-  const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'bottom',
-        },
-        title: {    
-          display: true,
-          text: 'Films sortis par mois par année'
-        }
-      }
-    },
-  }
-
-  const DISPLAYGRAPH = document.getElementById('displayGraph')
-  chart = new Chart(DISPLAYGRAPH, config)
-
-  updateChartWithRealData() // on lance la fonction une première fois pour que le graphe se charge une première fois sans que ça reste vide
-
-  let interval = setInterval(() => {
-    yearToShow++
-
-    if(yearToShow === ACTUALYEAR){
-        clearInterval(interval)
-        console.log("Fin de l'affichage")
-    }
-
-    updateChartWithRealData()
-  }, 11600) // pour que ça se synchronise avec avec l'effet du slider
-})
